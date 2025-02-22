5c0af08be9344b9890dbcbc8947c929282e6bd59
soicchi <soicchi@icloud.com>
2025-02-23 00:39:17 +0900

    feat: added a process without buffer channel

--- internal/goroutine/goroutine.go
+++ internal/goroutine/goroutine.go
@@ -2,8 +2,30 @@ package goroutine
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+
+
 func Goroutine() {
-	fmt.Println("goroutine called")
+	withNoBuffer()
+}
+
+func withNoBuffer() {
+	// with no buffer
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		ch <- 1
+		time.Sleep(5 * time.Second)
+		fmt.Println("completed sending")
+	}()
+
+	fmt.Println(<-ch)
+
+	wg.Wait()
 }
